fb2a3fd5ce5866bc83d38f3608bcbe89771b0f08
habiba-arache <habiba.arache@gmail.com>
2026-07-12 21:47:41 +0100

    boiler plate

---
+++ src/main/java/com/leet/Exercice.java
@@ -0,0 +1,7 @@
+package com.leet;
+
+public class Exercice {
+    public void method() {
+        // implement
+    };
+}
---
+++ src/test/java/ExerciceTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExerciceTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
