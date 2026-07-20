de817dc7da39e01419b9c7cd0a9e1f67af6a0d6d
habiba-arache <habiba.arache@gmail.com>
2026-07-20 22:42:47 +0100

    uncompleted exercice

--- src/main/java/com/leet/Solution.java
+++ src/main/java/com/leet/Solution.java
@@ -1,7 +1,37 @@
 package com.leet;
 
 public class Solution {
-    public void method() {
-        // implement
-    };
+    public String longestPalindrome(String s) {
+        if (s.length() == 1)
+            return s;
+        String rev = "";
+        for (int i = 0; i < s.length(); i++) {
+            rev += s.charAt(s.length() - i - 1);
+        }
+        int[][] matrix = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < rev.length(); j++) {
+                if (s.charAt(i) == rev.charAt(j))
+                    if (i == 0 || j == 0)
+                        matrix[i][j] = 1;
+                    else
+                        matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                else
+                    matrix[i][j] = 0;
+            }
+        }
+        int max = -1;
+        int index = -1;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                    index = i;
+                }
+            }
+        }
+        if (index != -1 && max != -1)
+            return s.substring(index - max + 1, index + 1);
+        return s.charAt(0) + "";
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -2,10 +2,26 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.leet.Solution;
+
 public class SolutionTest {
 
     @Test
     public void test() {
-        assertEquals(1, 1);
+        Solution s = new Solution();
+        String res = s.longestPalindrome("babad");
+        assertEquals("bab", res);
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        String res = s.longestPalindrome("cbbd");
+        assertEquals("bb", res);
+    } @Test
+    public void test3() {
+        Solution s = new Solution();
+        String res = s.longestPalindrome("aacabdkacaa");
+        assertEquals("aca", res);
     }
 }
