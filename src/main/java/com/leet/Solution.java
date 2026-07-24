bd27462ca7e09ba2b33d9a36fcadb87601554849
habiba-arache <habiba.arache@gmail.com>
2026-07-24 12:10:36 +0100

    Reverse integer

--- src/main/java/com/leet/Solution.java
+++ src/main/java/com/leet/Solution.java
@@ -1,7 +1,17 @@
 package com.leet;
 
 public class Solution {
-    public void method() {
-        // implement
-    };
+    public int reverse(int x) {
+        StringBuilder rev = new StringBuilder("0");
+        boolean flag = x < 0;
+        while (x != 0) {
+            rev.append(Math.abs(x % 10));
+            x = x / 10;
+        }
+        long l = Long.parseLong(rev.toString());
+
+        if ((int) l != l)
+            return 0;
+        return flag ? Integer.parseInt("-" + rev) : Integer.parseInt(rev + "");
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -2,10 +2,25 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.leet.Solution;
+
 public class SolutionTest {
 
     @Test
     public void test() {
-        assertEquals(1, 1);
+        Solution s = new Solution();
+        assertEquals(321, s.reverse(123));
+    }
+
+    @Test
+    public void test1() {
+        Solution s = new Solution();
+        assertEquals(-321, s.reverse(-123));
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        assertEquals(21, s.reverse(120));
     }
 }
