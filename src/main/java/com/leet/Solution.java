68a974281390f8a608771ad3e375009569efc230
habiba-arache <habiba.arache@gmail.com>
2026-07-25 03:11:30 +0100

    String to int (atoi)

--- src/main/java/com/leet/Solution.java
+++ src/main/java/com/leet/Solution.java
@@ -1,8 +1,30 @@
 package com.leet;
 
 public class Solution {
-    public int method() {
-        // implement
-        return 1;
-    };
+    public int myAtoi(String src) {
+        boolean isEmpty = true;
+        int sign = 1, intResult = 0, digit = 0;
+
+        for (char character : src.toCharArray()) {
+            if (isEmpty && character == '+')
+                isEmpty = false;
+            else if (isEmpty && character == '-') {
+                isEmpty = false;
+                sign = -1;
+            } else if (character >= '0' && character <= '9')// digit
+            {
+                digit = character - '0';
+                if (intResult > (Integer.MAX_VALUE - digit) / 10)
+                    return Integer.MAX_VALUE;
+
+                if (intResult < (Integer.MIN_VALUE + digit) / 10)
+                    return Integer.MIN_VALUE;
+                intResult = (intResult * 10) + (sign * digit);
+                isEmpty = false;
+            } else if (character != ' ' || (character == ' ' && !isEmpty))
+                break;
+        }
+        return intResult;
+    }
+
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -7,8 +7,68 @@ import com.leet.Solution;
 public class SolutionTest {
 
     @Test
+    public void test0() {
+        Solution s = new Solution();
+        assertEquals(2147483646, s.myAtoi("2147483646"));
+    }
+
+    @Test
     public void test() {
         Solution s = new Solution();
-        assertEquals(1, s.method());
+        assertEquals(42, s.myAtoi("42"));
+    }
+
+    @Test
+    public void test1() {
+        Solution s = new Solution();
+        assertEquals(-42, s.myAtoi(" -042"));
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        assertEquals(1337, s.myAtoi("1337c0d3"));
+    }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        assertEquals(0, s.myAtoi("0-1"));
+    }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        assertEquals(0, s.myAtoi("words and 987"));
+    }
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        assertEquals(2147483647, s.myAtoi(" 4294967296"));
+    }
+
+    @Test
+    public void test6() {
+        Solution s = new Solution();
+        assertEquals(-2147483648, s.myAtoi(" -24294967296"));
+    }
+
+    @Test
+    public void test7() {
+        Solution s = new Solution();
+        assertEquals(0, s.myAtoi(" +-12"));
+    }
+
+    @Test
+    public void test8() {
+        Solution s = new Solution();
+        assertEquals(0, s.myAtoi("   +0 123"));
+    }
+
+    @Test
+    public void test9() {
+        Solution s = new Solution();
+        assertEquals(2147483647, s.myAtoi("18446744073709551617"));
     }
 }
