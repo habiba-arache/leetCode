bd16316e2c392ed7696c3c93680aaeed64aca508
habiba-arache <habiba.arache@gmail.com>
2026-07-25 14:28:25 +0100

    isPalindrom

--- src/main/java/com/leet/Solution.java
+++ src/main/java/com/leet/Solution.java
@@ -1,8 +1,25 @@
 package com.leet;
 
 public class Solution {
-    public int method() {
-        // implement
-        return 1;
-    };
+    public boolean isPalindrome(int x) {
+        int length = 0, a = x, j;
+        if (a < 0)
+            return false;
+        if (a < 10)
+            return true;
+        while (a > 0) {
+            length++;
+            a /= 10;
+        }
+        j = (int) Math.pow(10, length - 1);
+        while (length >= 1) {
+            if (x % 10 != x / j)
+                return false;
+            x = x % j;
+            x /= 10;
+            length -= 2;
+            j /= 100;
+        }
+        return true;
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -9,6 +9,43 @@ public class SolutionTest {
     @Test
     public void test() {
         Solution s = new Solution();
-        assertEquals(1, s.method());
+        assertEquals(true, s.isPalindrome(9999));
+    }
+
+    @Test
+    public void test8() {
+        Solution s = new Solution();
+        assertEquals(false, s.isPalindrome(100));
+    }
+
+    @Test
+    public void test1() {
+        Solution s = new Solution();
+        assertEquals(false, s.isPalindrome(100003001));
+    }
+
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        assertEquals(true, s.isPalindrome(1234321));
+    }
+ 
+
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        assertEquals(false, s.isPalindrome(1000021));
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        assertEquals(false, s.isPalindrome(-121));
+    }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        assertEquals(false, s.isPalindrome(10));
     }
 }
