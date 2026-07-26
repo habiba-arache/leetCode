e1d0a976f088b3c697e1bccbe17815d64bd7f1a7
habiba-arache <habiba.arache@gmail.com>
2026-07-26 11:19:02 +0100

    sqrt function

--- src/main/java/com/leet/Solution.java
+++ src/main/java/com/leet/Solution.java
@@ -1,8 +1,22 @@
 package com.leet;
 
 public class Solution {
-    public int method() {
-        // implement
-        return 1;
+    public double sqrt(double x, double delta) {
+        if (x == 0)
+            return x;
+        return findSqrt(0, x, x, delta);
+    }
+
+    private double findSqrt(double start, double end, double target, double delta) {
+        double middle = (start + end) / 2;
+        double square = (middle * middle);
+        double absDiff = Math.abs(square - target);
+
+        if (absDiff <= delta)
+            return middle;
+        else if (square > target)
+            return findSqrt(start, middle, target, delta);
+        else
+            return findSqrt(middle, end, target, delta);
     };
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -6,9 +6,47 @@ import com.leet.Solution;
 
 public class SolutionTest {
 
+    private static final double DELTA = 0.002;
+
+    private final Solution solution = new Solution();
+
     @Test
-    public void test() {
-        Solution s = new Solution();
-        assertEquals(1, s.method());
+    public void testSqrt0() {
+        assertEquals(Math.sqrt(0), solution.sqrt(0, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt1() {
+        assertEquals(Math.sqrt(1), solution.sqrt(1, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt2() {
+        assertEquals(Math.sqrt(2), solution.sqrt(2, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt3() {
+        assertEquals(Math.sqrt(3), solution.sqrt(3, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt4() {
+        assertEquals(Math.sqrt(4), solution.sqrt(4, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt9() {
+        assertEquals(Math.sqrt(9), solution.sqrt(9, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt17() {
+        assertEquals(Math.sqrt(17), solution.sqrt(17, DELTA), DELTA);
+    }
+
+    @Test
+    public void testSqrt25() {
+        assertEquals(Math.sqrt(25), solution.sqrt(25, DELTA), DELTA);
     }
 }
