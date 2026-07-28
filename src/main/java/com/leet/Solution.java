743628bdd24eda41a8ac4bf2d8860dfc764dcc6c
habiba-arache <habiba.arache@gmail.com>
2026-07-28 15:26:06 +0100

    incompleted regex exercise

--- src/main/java/com/leet/Solution.java
+++ src/main/java/com/leet/Solution.java
@@ -1,8 +1,45 @@
 package com.leet;
 
 public class Solution {
-    public int method() {
-        // implement
-        return 1;
-    };
+    public boolean isMatch(String s, String reg) {
+        if (!reg.contains("*") && s.length() != reg.length())
+            return false;
+        int i = 0, j = 0;
+        while (i < s.length() && j < reg.length()) {
+            char regCharacter = reg.charAt(j);
+            char character = s.charAt(i);
+            if (regCharacter == character || regCharacter == '.') {
+                // accept any char if .
+                i++;
+                j++;
+            } else if (regCharacter == '*' && i >= 1 && j >= 1) {
+                // loop
+                char previousReg = reg.charAt(j - 1);
+                if (previousReg != '.') {
+                    char previous = s.charAt(i - 1);// a
+
+                    while (i < s.length() && s.charAt(i) == previous)
+                        i++;
+                    j++;
+                } else // .* in the word
+                if (j < reg.length() - 2) {
+                    j += 2;
+                    while (s.charAt(i) != reg.charAt(j))
+                        i++;
+                } else
+                    return true;
+
+            } else if (regCharacter != character) {
+                // "*" can cancel this char
+                if (j < reg.length() - 1 && reg.charAt(j + 1) == '*') {
+                    j += 2;
+                } else // normal case
+                    return false;
+
+            }
+        }
+        if (j == reg.length() && i == s.length())
+            return true;
+        return false;
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -9,6 +9,35 @@ public class SolutionTest {
     @Test
     public void test() {
         Solution s = new Solution();
-        assertEquals(1, s.method());
+        assertEquals(false, s.isMatch("aa", "a"));
+    }
+
+    @Test
+    public void test1() {
+        Solution s = new Solution();
+        assertEquals(true, s.isMatch("aa", "a*"));
+    }
+
+    @Test
+    public void test2() {
+        Solution s = new Solution();
+        assertEquals(true, s.isMatch("ab", ".*"));
+    }
+
+    @Test
+    public void test3() {
+        Solution s = new Solution();
+        assertEquals(true, s.isMatch("aab", "c*a*b"));
+    }
+    
+    @Test
+    public void test4() {
+        Solution s = new Solution();
+        assertEquals(false, s.isMatch("mississippi", "mis*is*p*."));
+    }
+    @Test
+    public void test5() {
+        Solution s = new Solution();
+        assertEquals(false, s.isMatch("ab", ".*c"));
     }
 }
